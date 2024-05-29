858e04b642f17690d557a324061133efcc47a346
Ernttimus <143626517+Ernttimus@users.noreply.github.com>
2024-05-29 11:18:44 +0200

    First commit creating react application with express + node

---
+++ client/index.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// ... continued from above - replace the `root.render(...)` line
+root.render(<Application />);
+
+function Application() {
+    const [counter, setCounter] = useState(0);
+    return <>
+        <h2>Welcome to my application</h2>
+        <div>
+            <button onClick={() => setCounter(oldValue => oldValue + 1)}>Click me</button>
+        </div>
+        <div>You have clicked {counter} times</div>
+    </>;
+}
