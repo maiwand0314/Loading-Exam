dc6998f1110ee287bfb855896fae9f9e48fbb858
Ernttimus <143626517+Ernttimus@users.noreply.github.com>
2024-06-06 02:19:58 +0200

    Edited App so we can test it with the modified test pages

--- client/App.jsx
+++ client/App.jsx
@@ -1,16 +1,24 @@
 
 import React from 'react'
-import UserPage from "./Pages/UserPage.jsx";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AdminPanel from "./Pages/AdminPanel";
+import InGameSwitch from "./PageTest/WaitingPageRouting2SwitchCase";
+import UserPage2 from "./PageTest/UserPage2";
+
+
 
 
 
 function App() {
     return(
         <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<UserPage />} />
-        </Routes>
+            <Routes>
+                <Route path="/" element={<UserPage2 />} />
+                <Route path="/admin" element={<AdminPanel />} />
+                <Route path="/gameID=1" element={<InGameSwitch />} />
+
+            </Routes>
         </BrowserRouter>
     );
 
