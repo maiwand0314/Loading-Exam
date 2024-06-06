3cb762dea2ecd8af4bd33c1c90a93e148ee01914
Ernttimus <143626517+Ernttimus@users.noreply.github.com>
2024-06-06 02:19:15 +0200

    Added WS

--- server/server.js
+++ server/server.js
@@ -1,12 +1,10 @@
 import express from "express";
 import * as path from "path";
-
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import * as bodyParser from "express";
-import {QuestionApi} from "./question.js";
-
-
+import { WebSocketServer } from "ws"; // Import WebSocketServer
+import { QuestionApi } from "./question.js";
 
 dotenv.config();
 const app = express();
@@ -20,7 +18,6 @@ const ListA = [];
 const ListB = [];
 const ListC = [];
 const ListD = [];
-
 
 function createResults() {
     const results = {
@@ -46,30 +43,44 @@ mongoClient.connect().then(async () => {
     const database = await mongoClient.db().admin().listDatabases();
     console.log(database);
     app.use("/api/elements", QuestionApi(mongoClient.db("Loading")));
-
 });
 
-app.use(express.static("../client/dist"));
-app.use((req, res, next) => {
-    if (req.method === "GET" && !req.path.startsWith("/api")) {
-        res.sendFile(path.resolve("../client/dist/index.html"));
-    } else {
-        next();
-    }
+// Set up WebSocket server
+const wsServer = new WebSocketServer({ noServer: true }); // Create a WebSocketServer instance
+
+// Handle incoming WebSocket connections
+const sockets = [];
+
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is running on port 3000");
+});
+
+// Handle incoming clients
+server.on("upgrade", (req, socket, head) => {
+    // This request is not passed through the middleware chain, so
+    // you have to duplicate any modifications to req here
+    wsServer.handleUpgrade(req, socket, head, (socket) => {
+        sockets.push(socket);
+        // Set up the handling of messages from this socket
+        socket.on("message", (msg) => {
+            const { option } = JSON.parse(msg); // Parse the received message
+            // Broadcast the selected option to all connected clients
+            for (const recipient of sockets) {
+                recipient.send(JSON.stringify({ option }));
+            }
+        });
+    });
 });
 
 
-
-
+// REST API endpoints
 app.get("/api/votes", async (req, res) => {
     res.json(createResults());
 });
 
-
 app.post("/api/votes/a", async (req, res) => {
     res.json(ListA);
 });
-
 
 app.get("/api/votes/a", async (req, res) => {
     res.json(ListA.length);
@@ -79,61 +90,31 @@ app.post("/api/votes/b", async (req, res) => {
     res.json(ListB);
 });
 
-
 app.get("/api/votes/b", async (req, res) => {
     res.json(ListB.length);
 });
-
-
-
 
 app.post('/api/choices', (req, res) => {
     const choice = req.body.choice;
     console.log('Received choice:', choice);
 
-
-
-
-
-    if (choice == "A"){
-
-    ListA.push(choice);
-
-    console.log("Choice A " + ListA.length)
+    if (choice == "A") {
+        ListA.push(choice);
+        console.log("Choice A " + ListA.length)
     }
 
-    if (choice == "B"){
-
+    if (choice == "B") {
         ListB.push(choice);
-
         console.log(ListB.length)
     }
 
-
-
-
-
     if (choice != null) {
-        voteList.push(choice); // Add the choice to the choices list
+        voteList.push(choice);
         counter++;
     }
 
-    // Log the whole list of choices
     console.log('Current list of choices:', voteList.toString());
     console.log('Number of votes:', counter);
 
-    // Here you can process the choice, save it to a database, or perform any other desired actions
-
     res.status(200).send();
 });
-
-
-
-
-
-
-
-
-
-
-app.listen(process.env.PORT || 3000);
