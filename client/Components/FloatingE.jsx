e2e1d1c59c6dacc4badeeb18b7a7271fd3318c5d
Ernttimus <143626517+Ernttimus@users.noreply.github.com>
2024-06-09 02:55:42 +0200

    Created component for emoji

---
+++ client/Components/FloatingE.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+
+function FloatingEmoji({ emoji }) {
+    const toggleSpawnAnimation = (event) => {
+        const emojiElement = event.currentTarget;
+
+        const spawnEmoji = document.createElement('div');
+        spawnEmoji.className = 'emoji floating-emoji';
+        spawnEmoji.textContent = emojiElement.textContent;
+
+        const x = Math.random() * (window.innerWidth - 40);
+        const y = Math.random() * (window.innerHeight - 40);
+
+        spawnEmoji.style.left = `${x}px`;
+        spawnEmoji.style.top = `${y}px`;
+
+        document.body.appendChild(spawnEmoji);
+
+        setTimeout(() => {
+            spawnEmoji.remove();
+        }, 5000);
+    };
+
+    return (
+        <p className="emoji" onClick={toggleSpawnAnimation}>
+            {emoji}
+        </p>
+    );
+}
+
+export default FloatingEmoji;
