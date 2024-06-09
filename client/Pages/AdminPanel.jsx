37b248adc8b4e30aa84483bce485d8200ce06886
Ernttimus <143626517+Ernttimus@users.noreply.github.com>
2024-06-09 15:00:05 +0200

    Made list into buttons and added cycle from component to question phase

--- client/Pages/AdminPanel.jsx
+++ client/Pages/AdminPanel.jsx
@@ -1,42 +1,46 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import "../Css/AdminPanel.css";
+import StartPageCycleButton from "../Components/StartPageCycleButton";
+import ResetVotesButton from "../Components/ResetVotesButton";
 
 
 const AdminPanel = () => {
     const [socket, setSocket] = useState(null);
-    const [currentState, setCurrentState] = useState()
+    const [currentState, setCurrentState] = useState(null);
+
     useEffect(() => {
         // Connect to WebSocket server when component mounts
         const newSocket = new WebSocket('ws://localhost:3000');
         setSocket(newSocket);
+
+        // Clean up function to close WebSocket connection when component unmounts
+        return () => newSocket.close();
     }, []);
 
-
-    const handleChooseScene = (handleButtonClick) => {
-        console.log("Sending selected option:", handleButtonClick);
+    const handleChooseScene = (option, startCycle = false) => {
+        console.log("Sending selected option:", option);
         if (socket) {
-            setCurrentState(handleButtonClick)
+            setCurrentState(option);
             // Send selected option to WebSocket server
-            socket.send(JSON.stringify({ option: handleButtonClick }));
+            socket.send(JSON.stringify({ option }));
         } else {
             console.error("WebSocket connection not available or no option selected");
         }
     };
-
-
 
     return (
         <div className="admin-panel-container">
             <div className="admin-panel">
                 <h1 className="title">Theatre Play Admin Panel</h1>
                 <div className="controls">
-                    <button onClick={(event) => handleChooseScene("intermissionScreen", event)} className="btn">Intermission Page</button>
-                    <button onClick={(event) => handleChooseScene("waitingPage", event)} className="btn">Waiting Page</button>
-                    <button onClick={(event) => handleChooseScene("getReadyToVotePage", event)} className="btn">Choice Page</button>
-                    <button onClick={(event) => handleChooseScene("resultPage", event)} className="btn">Result Page</button>
-                    <button onClick={(event) => handleChooseScene("getReadyToVoteSecond2", event)} className="btn">Brown Version Choice Page</button>
-                    <button onClick={(event) => handleChooseScene("resultPageBrownVersion", event)} className="btn">Brown Version Result Page</button>
-                    <button onClick={(event) => handleChooseScene("EndingPageMaiwand", event)} className="btn">Ending Page</button>
+                    <button onClick={() => handleChooseScene("intermissionScreen")} className="btn">Intermission Page</button>
+                    <button onClick={() => handleChooseScene("waitingPage")} className="btn">Waiting Page</button>
+                    <StartPageCycleButton handleChooseScene={handleChooseScene} />
+                    <button onClick={() => handleChooseScene("resultPage")} className="btn">Result Page</button>
+                    <button onClick={() => handleChooseScene("getReadyToVoteSecond2")} className="btn">Brown Version Choice Page</button>
+                    <button onClick={() => handleChooseScene("resultPageBrownVersion")} className="btn">Brown Version Result Page</button>
+                    <button onClick={() => handleChooseScene("EndingPageMaiwand")} className="btn">Ending Page</button>
+                    <ResetVotesButton />
                 </div>
             </div>
             <div className="current-state-box">
