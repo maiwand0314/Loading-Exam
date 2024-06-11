8dd1b639aa74113f0875e930dc4016dd5aed07f3
gitJoakim <156244356+gitJoakim@users.noreply.github.com>
2024-06-11 21:50:13 +0200

    Small style change to result bars (look at comment under ".result-bar{}" in css to undo :)

--- client/Components/ResultItem.jsx
+++ client/Components/ResultItem.jsx
@@ -3,7 +3,7 @@ const ResultItem = ({ amountOfVotes, choice, height }) => {
         <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
             <p>{amountOfVotes}</p>
             <div className="bar-container">
-                <div className="result-bar" style={{ height: height }}>
+                <div className="result-bar nes-container is-rounded" style={{ height: height }}>
                 </div>
             </div>
             <p>{choice}</p>
